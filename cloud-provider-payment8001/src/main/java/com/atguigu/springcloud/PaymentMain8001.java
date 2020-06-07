75b48a9433e1747bc972478e90446974218c6ac5
Always18YearsOld <lvtianbao@qq.com>
2020-06-07 15:06:42 +0800

    服务发现（只改了8001）

--- cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/PaymentMain8001.java
+++ cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/PaymentMain8001.java
@@ -3,6 +3,7 @@ package com.atguigu.springcloud;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
@@ -12,7 +13,9 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableDiscoveryClient
 public class PaymentMain8001 {
+    //上面的@EnableDiscoveryClient是服务发现的注解
     public static void main(String[] args) {
         SpringApplication.run(PaymentMain8001.class,args);
     }
--- cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/controller/PaymentController.java
+++ cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/controller/PaymentController.java
@@ -4,11 +4,13 @@ import com.atguigu.springcloud.entities.CommonResult;
 import com.atguigu.springcloud.entities.Payment;
 import com.atguigu.springcloud.service.PaymentService;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
+import java.util.List;
 
 /**
  * @author ：ltb
@@ -21,32 +23,62 @@ public class PaymentController {
     @Resource
     private PaymentService paymentService;
 
+
+    /**
+     * 设置这个服务的端口号
+     */
     @Value("${server.port}")
     private String serverPort;
 
+
+    /**
+     * 服务发现
+     */
+    @Resource
+    private DiscoveryClient discoveryClient;
+
+
     @PostMapping(value = "/payment/create")
-    public CommonResult create(@RequestBody Payment payment){
+    public CommonResult create(@RequestBody Payment payment) {
         int result = paymentService.create(payment);
         log.info("*****插入结果:" + result);
 
-        if (result>0){
-            return new CommonResult(200,"插入数据库成功,serverPort:"+serverPort,result);
-        }else{
-            return new CommonResult(444,"插入数据库失败",null);
+        if (result > 0) {
+            return new CommonResult(200, "插入数据库成功,serverPort:" + serverPort, result);
+        } else {
+            return new CommonResult(444, "插入数据库失败", null);
         }
 
     }
 
     @GetMapping(value = "/payment/get/{id}")
-    public CommonResult getPaymentById(@PathVariable("id") Long id){
+    public CommonResult getPaymentById(@PathVariable("id") Long id) {
         Payment payment = paymentService.getPaymentById(id);
         log.info("*****插入结果:" + payment);
 
-        if (payment!=null){
-            return new CommonResult(200,"查询成功,serverPort"+serverPort,payment);
-        }else{
-            return new CommonResult(444,"没有对应记录，查询ID：" + id,null);
+        if (payment != null) {
+            return new CommonResult(200, "查询成功,serverPort" + serverPort, payment);
+        } else {
+            return new CommonResult(444, "没有对应记录，查询ID：" + id, null);
         }
 
+    }
+
+
+    @GetMapping(value = "/payment/discovery")
+    public Object discovery() {
+
+        //获取下面有多少服务
+        List<String> services = discoveryClient.getServices();
+        for (String service : services) {
+            log.info("******services" + service);
+        }
+
+        //获取某一服务下面有多少实例
+        List<ServiceInstance> instances = discoveryClient.getInstances("CLOUD-PAYMENT-SERVICE");
+        for (ServiceInstance instance : instances) {
+            log.info(instance.getServiceId()+"\t"+instance.getHost()+"\t"+instance.getPort()+"\t"+instance.getUri());
+        }
+        return this.discoveryClient;
     }
 }
